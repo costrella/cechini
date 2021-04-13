acaab41472d3b02ba99ba8aec6e2da2a7b8f9a2b
Michal Kostrzewa <michal.kostrzewa89@gmail.com>
2021-04-13 17:06:51 +0200

    ApplicationProperties : cc

--- web/src/main/java/com/costrella/cechini/config/ApplicationProperties.java
+++ web/src/main/java/com/costrella/cechini/config/ApplicationProperties.java
@@ -10,4 +10,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  */
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
+    private String cc;
+
+    public String getCc() {
+        return cc;
+    }
+
+    public void setCc(String cc) {
+        this.cc = cc;
+    }
 }
