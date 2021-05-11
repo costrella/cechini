5c3f97913617ed28e629d22d223a1a8499795ef1
Michal Kostrzewa <michal.kostrzewa89@gmail.com>
2021-05-11 18:41:25 +0200

    package json

--- src/main/webapp/app/entities/stats/stats.component.ts
+++ src/main/webapp/app/entities/stats/stats.component.ts
@@ -1,9 +1,11 @@
 import { HttpResponse } from '@angular/common/http';
 import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { IChart01 } from 'app/shared/model/chart01.model';
 import { IStats } from 'app/shared/model/stats.model';
 import { JhiEventManager } from 'ng-jhipster';
 import { Subscription } from 'rxjs';
 import { StatsService } from './stats.service';
+import * as Chart from 'chart.js';
 
 @Component({
   selector: 'jhi-stats',
@@ -19,87 +21,87 @@ export class StatsComponent implements OnInit, OnDestroy, AfterViewInit {
 
   constructor(protected statsService: StatsService, protected eventManager: JhiEventManager) {}
 
-  // chart01(): void {
-  //   this.canvas = document.getElementById('myChart');
-  //   this.ctx = this.canvas.getContext('2d');
-  //   // eslint-disable-next-line no-unused-vars
+  chart01(): void {
+    this.canvas = document.getElementById('myChart');
+    this.ctx = this.canvas.getContext('2d');
+    // eslint-disable-next-line no-unused-vars
 
-  //   this.statsService.chart01().subscribe(
-  //     (res: HttpResponse<IChart01>) => {
-  //       const months = res.body?.monthsName;
+    this.statsService.chart01().subscribe(
+      (res: HttpResponse<IChart01>) => {
+        const months = res.body?.monthsName;
 
-  //       const myChart = new Chart(this.ctx, {
-  //         type: 'line',
-  //         data: {
-  //           labels: months,
-  //           datasets: res.body?.details,
-  //         },
-  //         options: {
-  //           scales: {
-  //             yAxes: [
-  //               {
-  //                 ticks: {
-  //                   beginAtZero: true,
-  //                 },
-  //               },
-  //             ],
-  //           },
-  //           legend: {
-  //             display: true,
-  //           },
-  //           title: {
-  //             display: true,
-  //             text: 'Wykres ilości przesłanych raportów',
-  //           },
-  //         },
-  //       });
-  //     },
-  //     () => this.onError()
-  //   );
-  // }
+        const myChart = new Chart(this.ctx, {
+          type: 'line',
+          data: {
+            labels: months,
+            datasets: res.body?.details,
+          },
+          options: {
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    beginAtZero: true,
+                  },
+                },
+              ],
+            },
+            legend: {
+              display: true,
+            },
+            title: {
+              display: true,
+              text: 'Wykres ilości przesłanych raportów',
+            },
+          },
+        });
+      },
+      () => this.onError()
+    );
+  }
 
-  // chart02(): void {
-  //   this.canvas02 = document.getElementById('myChart02');
-  //   this.ctx02 = this.canvas02.getContext('2d');
-  //   // eslint-disable-next-line no-unused-vars
+  chart02(): void {
+    this.canvas02 = document.getElementById('myChart02');
+    this.ctx02 = this.canvas02.getContext('2d');
+    // eslint-disable-next-line no-unused-vars
 
-  //   this.statsService.chart02().subscribe(
-  //     (res: HttpResponse<IChart01>) => {
-  //       const months = res.body?.monthsName;
+    this.statsService.chart02().subscribe(
+      (res: HttpResponse<IChart01>) => {
+        const months = res.body?.monthsName;
 
-  //       const myChart = new Chart(this.ctx02, {
-  //         type: 'line',
-  //         data: {
-  //           labels: months,
-  //           datasets: res.body?.details,
-  //         },
-  //         options: {
-  //           scales: {
-  //             yAxes: [
-  //               {
-  //                 ticks: {
-  //                   beginAtZero: true,
-  //                 },
-  //               },
-  //             ],
-  //           },
-  //           legend: {
-  //             display: true,
-  //           },
-  //           title: {
-  //             display: true,
-  //             text: 'Wykres ilości przesłanych zamówień',
-  //           },
-  //         },
-  //       });
-  //     },
-  //     () => this.onError()
-  //   );
-  // }
+        const myChart = new Chart(this.ctx02, {
+          type: 'line',
+          data: {
+            labels: months,
+            datasets: res.body?.details,
+          },
+          options: {
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    beginAtZero: true,
+                  },
+                },
+              ],
+            },
+            legend: {
+              display: true,
+            },
+            title: {
+              display: true,
+              text: 'Wykres ilości przesłanych zamówień',
+            },
+          },
+        });
+      },
+      () => this.onError()
+    );
+  }
 
   ngAfterViewInit(): void {
-    // this.chart01();
-    // this.chart02();
+    this.chart01();
+    this.chart02();
   }
 
   loadAll(): void {
